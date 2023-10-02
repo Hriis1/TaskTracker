7dda9ea5438c135c4016d56a2f2227168448b2b1
hrishd <hrishd@abv.bg>
2023-10-02 18:56:55 +0300

    asd

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ include_once("include/functions.php");
                 </div>
             </header>
 
-            <form action="" class="row">
+            <form id="form-new" action="" class="row">
                 <div class="col-sm-10">
                     <input type="text" class="form-control" placeholder="Enter new task name...">
                 </div>
@@ -51,8 +51,8 @@ include_once("include/functions.php");
                         <td>Data</td>
                         <td>Data</td>
                         <td>Data</td>
-                        <td><button class="btn btn-primary">Stop</button></td>
-                        <td><button class="btn btn-danger">X</button></td>
+                        <td><button class="btn btn-primary"><?php createIconLineHeight("stop", 1.5)?></button></td>
+                        <td><button class="btn btn-danger"><?php createIconLineHeight("times", 1.5)?></button></td>
                     </tr>
                 </tbody>
             </table>
@@ -62,7 +62,7 @@ include_once("include/functions.php");
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
+    <script src="js/tracker.js"></script>
 </body>
 
 </html>
